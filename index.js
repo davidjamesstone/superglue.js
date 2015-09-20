deb2b7115cfb89ffff502a63861f20071bb3a124
David Stone <davidjamesstone@gmail.com>
2015-09-20 22:21:49 +0100

    Update

---
+++ index.js
@@ -0,0 +1,13 @@
+// 'use strict'
+//
+// var patch = require('incremental-dom').patch
+//
+// function superglue (el, onRender) {
+//   this.el = el
+//   this.onRender = onRender
+// }
+// Superglue.prototype.render = function () {
+//   patch(this.el, this.onRender)
+// }
+//
+// module.exports = superglue
