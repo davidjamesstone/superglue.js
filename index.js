e389bf1693c9f4cdb6b2438c5379ac2aa758e2c3
David Stone <davidjamesstone@gmail.com>
2015-09-20 22:26:38 +0100

    Added email example

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-var IncrementalDOM = require('./incremental-dom')
-
-var patch = IncrementalDOM.patch
-var elementOpen = IncrementalDOM.elementOpen
-var elementVoid = IncrementalDOM.elementVoid
-var elementClose = IncrementalDOM.elementClose
-var text = IncrementalDOM.text
-
 var basket = require('./basket')
 var todo = require('./todo')
-
-window.patch = patch
-window.elementOpen = elementOpen
-window.elementVoid = elementVoid
-window.elementClose = elementClose
-window.text = text
+var emailClient = require('./email')
 
 window.basket = basket
 window.todo = todo
+window.emailClient = emailClient
